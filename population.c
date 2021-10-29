77b73715974d875901876b274897ea5e858c7d7c
swaranrajlaxmi <swaranrajlaxmi@users.noreply.github.com>
2021-10-29 12:14:21 +0000

    automated commit by submit50

--- population.c
+++ population.c
@@ -2,30 +2,28 @@
 # include <cs50.h>
 int main(void)
 {
-   int start_size;
-   int end_size;
-   int n=0;
-  do
-  {
-      start_size = get_int("Start size: ");
-  }
-  while (start_size < 9);
-  
-  do
-  {
-      end_size = get_int("End size: ");
-  }
-  while (end_size < start_size);
-  
+
+    int start_size, end_size, n = 0;
+    //prompt the user for Start size untill the given condition is met
+    do
+    {
+        start_size = get_int("Start size: ");
+    }
+    while (start_size < 9);
  
-      
-  
-  
-  while (start_size < end_size)
-  {
-      start_size = start_size + start_size/3 - start_size/4;
-      n++;
-  }
-  
-  printf("Years: %d \n", n);
+    //prompt the user for End size untill the given condition is met
+    do
+    {
+        end_size = get_int("End size: ");
+    }
+    while (end_size < start_size);
+ 
+    //calculate the number of years required for the population to reach at least the size of the end value.
+    while (start_size < end_size)
+    {
+        start_size = start_size + start_size / 3 - start_size / 4;
+        n++;
+    }
+    //print the number of years
+    printf("Years: %d \n", n);
 }
