f2effb2b8368f4b0bcc12a05d078d114604f49ec
swaranrajlaxmi <swaranrajlaxmi@users.noreply.github.com>
2021-11-07 09:31:31 +0000

    automated commit by submit50

--- cash.c
+++ cash.c
@@ -2,42 +2,43 @@
 # include <cs50.h>
 # include <math.h>
 
-int main (void)
+int main(void)
 {
     float dollar;
     //prompt user for change owed
     do
     {
-        dollar = get_float ("change owed: ");
+        dollar = get_float("change owed: ");
     }
     while (dollar <= 0);
     //converting the user’s inputted dollars to cents
-    int cents = round (dollar * 100);
+    int cents = round(dollar * 100);
     int coins = 0;
+
     while (cents >= 1)
     {
-    if (cents >= 25)
-    {
-        cents = cents - 25;
-        coins++;
-    }
-    else if (cents >= 10)
-    {
-        cents = cents - 10;
-        coins++;
-    }
-    else if (cents >= 5)
-    {
-        cents = cents - 5;
-        coins++;
-    }
-    else if (cents >= 1)
-    {
-        cents = cents - 1;
-        coins++;
-    }
+        if (cents >= 25)
+        {
+            cents = cents - 25;
+            coins++;
+        }
+        else if (cents >= 10)
+        {
+            cents = cents - 10;
+            coins++;
+        }
+        else if (cents >= 5)
+        {
+            cents = cents - 5;
+            coins++;
+        }
+        else if (cents >= 1)
+        {
+            cents = cents - 1;
+            coins++;
+        }
     }
     printf("%i\n", coins);
-    
-    
+
+
 }
