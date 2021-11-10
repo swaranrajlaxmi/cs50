6824553f0ab40215da82f9a5c101f10e525e578b
swaranrajlaxmi <swaranrajlaxmi@users.noreply.github.com>
2021-11-10 05:03:01 +0000

    automated commit by submit50

--- credit.c
+++ credit.c
@@ -10,21 +10,21 @@ int main(void)
     {
         Number = get_long("Number: ");
     }
-    while(Number < 1 || Number > 9999999999999999);
+    while (Number < 1 || Number > 9999999999999999);
     // Apply luhn's algorithm for checksum
     long dividend = Number;
     int count = 0;
     int modulus;
     int sum;
-    
-    while(dividend > 0)
+    // check validity of number and credit length simultaneuosly. 
+    while (dividend > 0)
     {
         modulus = dividend % 10;
         count++;
-
-        if(count % 2 == 0)
+        // if counter is even then remaider will multiply by 2 and check further conditions
+        if (count % 2 == 0)
         {
-            if(modulus * 2 > 9)
+            if (modulus * 2 > 9)
             {
                 sum = sum + ((modulus * 2) - 9);
             }
@@ -33,6 +33,7 @@ int main(void)
                 sum = sum + (modulus * 2);
             }
         }
+        //else simply add the remainder
         else
         {
             sum = sum + modulus;
@@ -41,37 +42,37 @@ int main(void)
         dividend = dividend / 10;
             
     }
-
-   long starting_digit = Number;
-   while(starting_digit > 99 || starting_digit < 9)
-   {
-       starting_digit = starting_digit / 10;
-   }
-   
-   if(sum % 10 != 0)
-   {
-       printf("INVALID\n");
-   }
-   
+    //check for starting digits
+    long starting_digit = Number;
+    while (starting_digit > 99 || starting_digit < 9)
+    {
+        starting_digit = starting_digit / 10;
+    }
+    // if not satisfying luhn's algorithm then card number is invalid
+    if (sum % 10 != 0)
+    {
+        printf("INVALID\n");
+    }
+    // else check below conditions and print accordingly.
     else
     {
-   
-            if(count == 16 && (starting_digit >= 51 && starting_digit <= 55))
-            {
-                printf("MASTERCARD\n");
-            }
-            else if(count == 15 && (starting_digit == 34 || starting_digit == 37))
-            {
-                printf("AMEX\n");
-            }
-            else if((count == 13 || count == 16) && starting_digit >= 40 && starting_digit <= 49)
-            {
-                printf("VISA\n");
-            }
-            else
-            {
-                printf("INVALID\n");
-            }
+        
+        if (count == 16 && (starting_digit >= 51 && starting_digit <= 55))
+        {
+            printf("MASTERCARD\n");
+        }
+        else if (count == 15 && (starting_digit == 34 || starting_digit == 37))
+        {
+            printf("AMEX\n");
+        }
+        else if ((count == 13 || count == 16) && starting_digit >= 40 && starting_digit <= 49)
+        {
+            printf("VISA\n");
+        }
+        else
+        {
+            printf("INVALID\n");
+        }
     }
     
     
