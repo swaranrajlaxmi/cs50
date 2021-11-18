d13bd6bce787d061abc539d2e38900ae87ff8825
swaranrajlaxmi <swaranrajlaxmi@users.noreply.github.com>
2021-11-18 04:56:38 +0000

    automated commit by submit50

--- caesar.c
+++ caesar.c
@@ -4,24 +4,26 @@
 # include <stdlib.h>
 # include <ctype.h>
 
-int main (int argc, string argv[])
+int main(int argc, string argv[])
 {
+    //check for single command line arguments
     if (argc != 2)
     {
         printf("Usage: ./caesar key\n");
         return 1;
     }
+    //check arguments to make sure all characters are digits
     for (int i = 0, n = strlen(argv[1]); i < n;)
     {
-            if(isdigit(argv[1][i]))
-            {
-                i++;
-            }
-            else
-            {
-                printf("Usage: ./caesar key\n");
-                return 1;
-            }
+        if (isdigit(argv[1][i]))
+        {
+            i++;
+        }
+        else
+        {
+            printf("Usage: ./caesar key\n");
+            return 1;
+        }
         
     }
     //TODO: Convert the command-line argument from a string to an int
@@ -32,7 +34,7 @@ int main (int argc, string argv[])
     printf("ciphertext: ");
    
     
-   //TODO: Iterate over each character of the plaintext
+    //TODO: Iterate over each character of the plaintext
 
     int n = strlen(text);
     for (int i = 0; i < n; i++)
@@ -40,7 +42,7 @@ int main (int argc, string argv[])
         //TODO: If it is an uppercase letter, rotate it, then print 
         if (isupper(text[i]))
         {
-            text[i] = ((text[i] - 65) +k) % 26 + 65;
+            text[i] = ((text[i] - 65) + k) % 26 + 65;
             printf("%c", text[i]);
         }
         //TODO: If it is a lowercase letter, rotate it, then print 
@@ -57,46 +59,3 @@ int main (int argc, string argv[])
     }
     printf("\n");
 }
-/*{
-    //TODO: check for single command line argument, and make sure all characters are in digit.
-    if (argc == 2 && isdigit(*argv[1]))
-    {
-            //TODO: Convert the command-line argument from a string to an int
-            int k = atoi(argv[1]);
-            //TODO: get user text
-            string text = get_string("plaintext:  ");
-            //TODO: print cipher text
-            printf("ciphertext: ");
-            
-            //TODO: Iterate over each character of the plaintext
-            int n = strlen(text);
-            for (int i = 0; i < n; i++)
-            {
-                //TODO: If it is an uppercase letter, rotate it, then print 
-                if (isupper(text[i]))
-                {
-                    text[i] = ((text[i] - 65) +k) % 26 + 65;
-                    printf("%c", text[i]);
-                }
-                //TODO: If it is a lowercase letter, rotate it, then print 
-                else if (islower(text[i]))
-                {
-                    text[i] = ((text[i] - 97) + k) % 26 + 97;
-                    printf("%c", text[i]);
-                }
-                //TODO: If it is neither, print out the character as is
-                else
-                {
-                    printf("%c", text[i]);
-                }
-                printf("\n");
-                return 0;
-            }
-        
-    }
-    else
-    {
-        printf("Usage: ./caesar key\n");
-        return 1;
-    }
-}*/
