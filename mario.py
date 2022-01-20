db7ffcf323b084c8b5dc3df14387aa681270d7b2
swaranrajlaxmi <swaranrajlaxmi@users.noreply.github.com>
2022-01-20 05:08:08 +0000

    automated commit by submit50

--- mario.py
+++ mario.py
@@ -2,26 +2,29 @@
 
 import importlib
 
+
 def main():
     height = get_height()
+    # printing hash as like right alighned pyramid
     for i in range(height):
         for j in range(height):
-            if j<(height-(i+1)):
+            if j < (height-(i+1)):
                 print(" ", end="")
             else:
                 print("#", end="")
         print()
 
 
-
 def get_height():
+    # prompt the user for height untill the given condition is met
     while True:
         try:
             n = int(input("Height: "))
-            if n>0 and n<=8:
+            if n > 0 and n <= 8:
                 break
         except ValueError:
             print("That's not an integer!")
     return n
+
 
 main()
