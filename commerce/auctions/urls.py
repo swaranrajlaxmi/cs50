2c8fb6ba09f8432a592a980346ea4a42d621bb91
Swaran <swaranrajlaxmi@gmail.com>
2022-06-14 20:19:57 +0800

    adds all new files

--- commerce/auctions/urls.py
+++ commerce/auctions/urls.py
@@ -2,9 +2,20 @@ from django.urls import path
 
 from . import views
 
+app_name = 'auctions'
+
 urlpatterns = [
     path("", views.index, name="index"),
     path("login", views.login_view, name="login"),
     path("logout", views.logout_view, name="logout"),
-    path("register", views.register, name="register")
+    path("register", views.register, name="register"),
+    path("user_panel", views.user_panel, name="user_panel"),
+    path("create_listing", views.create_listing, name="create_listing"),
+    path("<int:auction_id>", views.listing_page, name="listing_page"),
+    path("watchlist", views.watchlist, name="watchlist"),
+    path("bid", views.bid, name="bid"),
+    path("categories", views.categories, name="categories"),
+    path("categories/<str:category>", views.categories, name="categories"),
+    path("close_auction/<str:auction_id>", views.close_auction, name="close_auction"),
+    path("handle_comment/<str:auction_id>", views.handle_comment, name="handle_comment")
 ]
