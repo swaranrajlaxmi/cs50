6b8866a04cfe5bcd28f42cf32527becf8f362e59
Swaran <swaranrajlaxmi@gmail.com>
2022-09-14 18:33:59 +0800

    adds expense html

--- expense_tracker/views.py
+++ expense_tracker/views.py
@@ -1,24 +1,67 @@
-from audioop import reverse
+from django.urls import reverse
+from sqlite3 import IntegrityError
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
+from django.contrib.auth import authenticate, login, logout
 
 from .models import User
 
 def index(request):
-    # if request.user.is_authenticated:
-    #     return render(request, "")
+    if request.user.is_authenticated:
+        return render(request, "expense.html")
 
-    # else:
-        # return HttpResponseRedirect(reverse("login"))
-        return render(request, "login.html")
+    else:
+        return HttpResponseRedirect(reverse("login"))
 
 
 def login_view(request):
+    if request.method == "POST":
+
+        username = request.POST["username"] 
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect(reverse("index"))
+    
+        else:
+            return render(request, "login.html", {
+                "message": "Invalid username and/or password."
+            })
     return render(request, "login.html")
 
 
 def register(request):
-    return render(request, "register.html")
+    if request.method == "POST":
+        username = request.POST["username"]
+        email = request.POST["email"]
+        password = request.POST["password"]
+        confirmation = request.POST["confirmation"]
+
+        if password != confirmation:
+            return render(request, "register.html",{
+                "message": "Password and Confirm Password should be same."
+            })
+
+        try:
+            user = User.objects.create_user(username, email, password)
+            # user = User()
+            # user.username = username
+            # user.email = email
+            # user.password = password
+            user.save()
+        except IntegrityError:
+            return render(request, "register.html", {
+                "message": "Username already taken."
+            })
+
+        login(request, user)
+        return HttpResponseRedirect(reverse("index"))
+    else:
+        return render(request, "register.html")
+
 
 def logout_view(request):
-    return render(request, "login.html")
+    logout(request)
+    return HttpResponseRedirect(reverse("index"))
